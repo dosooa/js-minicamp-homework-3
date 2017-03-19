4401e8fc4fc4f555721ede4c8493324f30cb2d91
Brian Burkard <brianburkard@Brians-MacBook-Pro.local>
2017-03-19 14:34:49 -0400

    homework

--- exercises.js
+++ exercises.js
@@ -1,5 +1,19 @@
 'use strict';
 
+/* Feynman writing.
+
+Objects - Like a variable, but stores a colllection of variables or functions inside it in pairs, like color: 'red', size: 'large', etc.
+
+Properties - The property(key) in an object is like a variable that has a value inside that object. like in color: red. 'color' is a property of whatever object it is inside of, and 'red is that properties value.
+
+Methods - A method is a function inside of an object. It can be called by calling the object name followed by the method: object.method();
+
+for in loop - Simialler to a for loop, the for in loop is used for iterating through properties(keys) inside of an object.
+
+dot notation vs bracket notation - Using dot notation is preferred and can be used whenever you need to access an objects property you know the name of. If the name of the 
+property is unknown or you are looping through multiple properties with a variable, you need to use [bracket] notation to access it. 
+
+*/
 //Do not change any of the function names
 
 function makeCat(name, age) {
@@ -7,61 +21,95 @@ function makeCat(name, age) {
   //add an age property to the object with the value set to the age argument
   //add a method called meow that returns the string 'Meow!'
   //return the object
+    var cat = {};
+  cat.name = name;
+  cat.age = age;
+  cat.meow = function(){
+    return 'Meow!';
+  };
+  return cat;
 }
 
 function addProperty(object, property) {
   //add the property to the object with a value of null
   //return the object
   //note: the property name is NOT 'property'.  The name is the value of the argument called property (a string)
+    object[property] = null;
+    return object;
 }
 
 function invokeMethod(object, method) {
   //method is a string that contains the name of a method on the object
   //invoke this method
   //nothing needs to be returned
+    object[method]();
 }
 
 function multiplyMysteryNumberByFive(mysteryNumberObject) {
   //mysteryNumberObject has a property called mysteryNumber
   //multiply the mysteryNumber property by 5 and return the product
+   return mysteryNumberObject.mysteryNumber *= 5;
 }
 
 function deleteProperty(object, property) {
   //remove the property from the object
   //return the object
+    delete object[property];
+    return object;
 }
 
 function newUser(name, email, password) {
   //create a new object with properties matching the arguments passed in.
   //return the new object
+    var user = {};
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    return user;
 }
 
 function hasEmail(user) {
   //return true if the user has a value for the property 'email'
   //otherwise return false
+    if (user.email){
+        return true;
+    }
+    return false;
 }
 
 function hasProperty(object, property) {
   //return true if the object has the value of the property argument
   //property is a string
   //otherwise return false
+    if (object[property]){
+        return true;
+    }
+    return false;
 }
 
 function verifyPassword(user, password) {
   //check to see if the provided password matches the password property on the user object
   //return true if they match
   //otherwise return false
+    if(user.password === password){
+        return true;
+    }
+    return false;
 }
 
 function updatePassword(user, newPassword) {
   //replace the existing password on the user object with the value of newPassword
   //return the object
+    user.password = newPassword;
+    return user;
 }
 
 function addFriend(user, newFriend) {
   //user has a property called friends that is an array
   //add newFriend to the end of the friends array
   //return the user object
+    user.friends.push(newFriend);
+    return user;
 }
 
 function setUsersToPremium(users) {
@@ -69,6 +117,10 @@ function setUsersToPremium(users) {
   //each user object has the property 'isPremium'
   //set each user's isPremium property to true
   //return the users array
+    for (var i = 0; i < users.length; i++){
+        users[i].isPremium = true;
+    }
+    return users;
 }
 
 function sumUserPostLikes(user) {
@@ -77,6 +129,11 @@ function sumUserPostLikes(user) {
   //each post object has an integer property called 'likes'
   //sum together the likes from all the post objects
   //return the sum
+    var added = 0;
+    for (var i = 0; i < user.posts.length; i++){
+        added += user.posts[i].likes;
+    }
+    return added;
 }
 
 function addCalculateDiscountPriceMethod(storeItem) {
@@ -87,6 +144,11 @@ function addCalculateDiscountPriceMethod(storeItem) {
   //price -> 20
   //discountPercentage -> .2
   //discountPrice = 20 - (20 * .2)
+    storeItem.calculateDiscountPrice = function(){
+     var discountPrice =  this.price - (this.price * this.discountPercentage);
+       return discountPrice;
+    };
+    return storeItem;
 }
 
 //Do not modify code below this line.
